b26c08d264c88be44baa580d0d00647b49fc86b8
Kyle Anderson <kyle@xkyle.com>
2024-10-02 13:03:10 -0700

    wip sensor

--- components/irobot_bridge/__init__.py
+++ components/irobot_bridge/__init__.py
@@ -1,11 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, mqtt
-from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_BATTERY,
+    UNIT_PERCENT,
+    ENTITY_CATEGORY_DIAGNOSTIC,
+    STATE_CLASS_MEASUREMENT,
+)
 
 CODEOWNERS = []
 DEPENDENCIES = []
-AUTO_LOAD = ["json", "network", "mqtt"]
+AUTO_LOAD = ["json", "network", "mqtt", "sensor"]
 MULTI_CONF = True
 
 CONF_IROBOT_BRIDGE = "irobot_bridge"
@@ -13,28 +19,43 @@ CONF_ADDRESS = "address"
 CONF_BLID = "blid"
 CONF_PASSWORD = "password"
 
-irobot_bridge_ns = cg.esphome_ns.namespace('irobot_bridge')
-IrobotBridge = irobot_bridge_ns.class_('Irobot_Bridge', cg.PollingComponent)
+CONF_BATTERY_PERCENT = "battery_percent"
 
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(IrobotBridge),
-            cv.Required(CONF_ADDRESS): cv.string,
-            cv.Required(CONF_BLID): cv.string,
-            cv.Required(CONF_PASSWORD): cv.string,
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
+irobot_bridge_ns = cg.esphome_ns.namespace("irobot_bridge")
+IrobotBridge = irobot_bridge_ns.class_(
+    "Irobot_Bridge",
+    cg.Component,
 )
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(IrobotBridge),
+        cv.Required(CONF_ADDRESS): cv.string,
+        cv.Required(CONF_BLID): cv.string,
+        cv.Required(CONF_PASSWORD): cv.string,
+        cv.Optional(CONF_BATTERY_PERCENT, default={"id": "battery_percent"}): sensor.sensor_schema(
+            unit_of_measurement=UNIT_PERCENT,
+            device_class=DEVICE_CLASS_BATTERY,
+            accuracy_decimals=0,
+            state_class=STATE_CLASS_MEASUREMENT,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     cg.add_define("USE_MQTT")
-    # cg.add_define("ASYNC_TCP_SSL_ENABLED", True)
-    # cg.add_define("SHA1_SIZE", 20)
+    cg.add_define("USE_SENSOR")
+
     var = cg.new_Pvariable(config[CONF_ID])
     cg.add(var.set_address(config[CONF_ADDRESS]))
     cg.add(var.set_blid(config[CONF_BLID]))
     cg.add(var.set_password(config[CONF_PASSWORD]))
     await cg.register_component(var, config)
+
+    if battery_percent_config := config.get(CONF_BATTERY_PERCENT):
+        sens = await sensor.new_sensor(battery_percent_config)
+        cg.add(var.set_battery_percent_sensor(sens))
+        cg.add(sens)
--- components/irobot_bridge/irobot_bridge.h
+++ components/irobot_bridge/irobot_bridge.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
 #include "esphome/components/mqtt/mqtt_client.h"
+#include "esphome/components/sensor/sensor.h"
 #include "ca_cert_chain.h"
 
 namespace esphome
@@ -30,11 +31,15 @@ namespace esphome
       void handle_json_message(const std::string &topic, const JsonObject doc);
       void handle_message(const std::string &topic, const std::string &payload);
 
+      void set_battery_percent_sensor(sensor::Sensor *sensor) { this->battery_percent = sensor; }
+
     protected:
       std::string address_;
       std::string blid_;
       std::string password_;
       esphome::mqtt::MQTTClientComponent *mqtt_client_;
+
+      sensor::Sensor *battery_percent{nullptr};
     };
 
   } // namespace irobot_bridge
